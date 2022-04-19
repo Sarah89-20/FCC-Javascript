6d179deea0ef29f5b9ecae38f67bb71c480ee67e
Sarah89-20 <76457319+Sarah89-20@users.noreply.github.com>
2022-04-18 23:18:41 -0500

    menu project started

---
+++ menu/menu.js
@@ -0,0 +1,34 @@
+const menu = [
+    {
+        id: 1,
+        title: "Buttermilk Pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpg",
+        desc: "stufffffffff",
+    },
+    {
+        id: 2,
+        title: "Blueberry Pancakes",
+        category: "breakfast",
+        price: 15.00,
+        img: "./images/item-2.jpg",
+        desc: "stufffffffff f",
+    },
+    {
+        id: 3,
+        title: "Banana Pancakes",
+        category: "breakfast",
+        price: 16.00,
+        img: "./images/item-3.jpg",
+        desc: "stufffffffff ff",
+    }, 
+    {
+        id: 4,
+        title: "Walnut Pancakes",
+        category: "breakfast",
+        price: 16.00,
+        img: "./images/item-4.jpg",
+        desc: "stufffffffff fff",
+    } 
+];
